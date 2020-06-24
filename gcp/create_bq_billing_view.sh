ea83b0461306669a3411b96e8ed38ae7440a35a7
Jeremy Campbell <jcampbell@spins.com>
2020-06-24 15:41:18 -0500

    Billing View Created

---
+++ gcp/create_bq_billing_view.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "${PROJECT}" -o -z "${DATASET}" ]; then
+  echo "Please set \$PROJECT and \$DATASET."
+  exit 1
+fi
+
+IFS='' read -d '' Q <<EOF
+#standardSQL
+/*
+ * Script: GCP Billing Export 
+ * Author: jeremyrcampb
+ * Source: https://github.com/jeremyrcampb/cloudcostmgmt/
+ * Description: 
+ * 
+ * Creates a user friendly view for querying the
+ * Google Cloud Platform Billing export from BigQuery.
+ */
+Select 
+  CASE
+     WHEN (SELECT value FROM UNNEST(labels) WHERE key = 'cost-center') != '' THEN (SELECT value FROM UNNEST(labels) WHERE key = 'cost-center')
+     ELSE (SELECT value FROM UNNEST(project.labels) WHERE key = 'cost-center')
+  END AS cost_center,
+  (SELECT SUM(amount) FROM UNNEST(credits)) as total_credits,
+  *
+FROM 
+  \`$PROJECT.$DATASET.gcp_billing_export_*\`;
+EOF
+
+bq mk --project $PROJECT --view="${Q}" $DATASET.gcp_billing
